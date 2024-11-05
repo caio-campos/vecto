452d92695c399d6c149bcb4c8bdba46481a998d8
Caio Campos <ocaioac7@gmail.com>
2024-11-04 22:21:25 -0300

    fix: sending null str in empty body

--- request_transformers.go
+++ request_transformers.go
@@ -6,10 +6,18 @@ import (
 )
 
 func ApplicationJsonReqTransformer(req Request) (data []byte, err error) {
+	if req.data == nil || req.data == "" {
+		return nil, nil
+	}
+
 	return json.Marshal(req.data)
 }
 
 func FormEncodedReqTransformer(req Request) (data []byte, err error) {
+	if req.data == nil || req.data == "" {
+		return nil, nil
+	}
+
 	paramsMap := make(map[string]string)
 	if req.data != nil {
 		dataMap, ok := req.data.(map[string]string)
